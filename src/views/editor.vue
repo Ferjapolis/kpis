2d68d6ed4149a207e498b4f6d3015c794f2dbab1
Fernando <barroso.ouharriet@gmail.com>
2021-10-13 20:42:08 -0300

    update

--- src/views/editor.vue
+++ src/views/editor.vue
@@ -1,94 +1,152 @@
 <template>
-    <v-container>
-    <v-row  class="pt-10">
-        <h3>Actualización de Valores</h3>
-        <v-spacer></v-spacer>
-        <Nuevokpi />
+  <v-container>
+    <v-row>
+      <v-col cols="1">
+        <v-btn @click="volver" icon><v-icon>mdi-arrow-left</v-icon></v-btn>
+      </v-col>
+      <v-col cols="2"><h2>{{$store.state.area}}</h2></v-col>
     </v-row>
     <v-row justify="center">
-        <v-col cols="3">
+      <v-col cols="3">
         <selectorkpi />
-        </v-col>
-        <v-col cols="3">
-        <h4 class="pt-6">Owner: <b>{{kpi.owner}}</b></h4>
-        </v-col>
+      </v-col>
+      <v-col cols="3">
+        <h4 class="pt-6">Owner: {{kpi.owner}}</h4>
+      </v-col>
+      <v-col cols="3">
+         <h5 class="pt-6">last update: {{kpi.update}}</h5>
+      </v-col>
     </v-row>
     <v-row justify="center">
-        <v-col cols="4">
+      <v-col cols="4">
         <Form />
-        </v-col>
-        <v-col cols="3">
-            <v-card
-              class="my-auto"
-              max-width="150"
-              outlined
-            >
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <div class="text-overline mb-1">
-                    YTD
-                  </div>
-                  <v-list-item-title class="text-h3 mb-1">
-                    {{calculo(kpi.calc_ytd)}}
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
-            <v-card
-              class="my-auto"
-              max-width="150"
-              outlined
-            >
-              <v-list-item three-line>
-                <v-list-item-content>
-                  <div class="text-overline mb-1">
-                    FSCT
-                  </div>
-                  <v-list-item-title class="text-h3 mb-1">
-                    {{calculo(kpi.calc_fsct)}}
-                  </v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
-        </v-col>
+      </v-col>
+      <v-col cols="3">
+        <div class="pb-3">
+          <v-card class="my-auto ma-3" width="150" outlined>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <div class="text-overline mb-1">
+                  YTD <b>({{this.kpi.calc_ytd}})</b>
+                </div>
+                <v-list-item-title class="text-h3 mb-1">
+                   0
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </div>
+        <div class="pb-3">
+        <v-card class="my-auto ma-3" width="150" outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="text-overline mb-1">
+                FSCT <b>({{this.kpi.calc_fsct}})</b>
+              </div>
+              <v-list-item-title class="text-h3 mb-1">
+                0
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+        </div>
+        <div class="pb-3">
+        <v-card class="my-auto ma-3" width="150" outlined>
+          <v-list-item three-line>
+            <v-list-item-content>
+              <div class="text-overline mb-1">
+                Regla
+              </div>
+              <v-list-item-title class="text-h3 mb-1">
+                {{this.kpi.rule}}
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+        </div>
+      </v-col>
+      <v-col cols="5">
+        <Chart :options="chartOptions"></Chart>
+      </v-col>
     </v-row>
-    </v-container>
+     <v-snackbar v-model="snack" :timeout="3000" :color="snackColor">
+      {{ snackText }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn v-bind="attrs" text @click="snack = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </v-container>
 </template>
 
 <script>
-import Form from '../components/form.vue'
-import selectorkpi from '../components/filtro.vue'
-import Nuevokpi from '../components/nuevokpi.vue'
+import Form from '../components/tablaKPI.vue'
+import selectorkpi from '../components/buscadorKPI.vue'
+import Chart from '../components/barras.vue'
 
 export default {
-  name: 'App',
+  name: 'editor',
   components: {
     Form,
     selectorkpi,
-    Nuevokpi
+    Chart
   },
   data: () => ({
     dialog: false,
-    kpi: {}
+    snack: false,
+    snackColor: '',
+    snackText: '',
+    kpi: {},
+    datos: [],
+    chartOptions: {
+      series: [{
+        data: [1, 2, 3] // sample data
+      }]
+    }
   }),
   watch: {
     '$store.state.kpi': function () {
       this.kpi = this.$store.state.kpi
+      this.datos = this.$store.state.datos
+    },
+    '$store.state.guardado': function () {
+      this.save()
+      this.$store.commit('estado', false)
+    }
+  },
+  computed: {
+    regularComputed () {
+      return this.calculo2()
     }
   },
   methods: {
-    calculo (tipo) {
-      const valor = 0
-      if (tipo === 'Max') {
-        return data.reduce ((min, b) => Math.min (min, by), data [0] .y)
+    volver () {
+      this.$router.push({ path: '/' })
+    },
+    save () {
+      this.snack = true
+      this.snackColor = 'success'
+      this.snackText = 'Data saved'
+    },
+    calculo2 () {
+      var valor = 0
+      if (this.kpi.calc_ytd === 'Max') {
+        var findTop = []
+        this.datos.forEach(obj => findTop.push(obj.valor))
+        console.log(findTop)
+        return Math.max(...findTop)
       }
-      if (tipo === 'Min') {
+      if (this.kpi.calc_ytd === 'Min') {
         return valor
       }
-      if (tipo === 'Avg') {
+      if (this.kpi.calc_ytd === 'Avg') {
         return valor
       }
-      if (tipo === 'Acum') {
+      if (this.kpi.calc_ytd === 'Acum') {
+        return valor
+      } else {
         return valor
       }
     }
